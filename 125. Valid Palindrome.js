5e3a9ec4716045f8ef2b675910a5ec76f495e0cd
sujana102119 <sujana102119@gmail.com>
2023-02-03 00:15:42 +0530

    Neetcode All - Day 16 - LC 125

---
+++ 125. Valid Palindrome.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    if (s.length === 1) return true;  
+    let start = 0;
+    let end = s.length - 1;
+    while(start < end) {
+        let startChar = s.charCodeAt(start);
+        let endChar = s.charCodeAt(end);
+        if(!isAlphanumeric(startChar)) {
+            start++;
+            continue;
+        } 
+        if (!isAlphanumeric(endChar)) {
+            end--;
+            continue;
+        }  
+        
+        if (toLower(startChar) !== toLower(endChar)) {
+                return false;
+        }  
+
+        start++;
+        end--;
+    } 
+    return true;
+};
+
+var isAlphanumeric = function (code) {
+    return  ((code > 47 && code < 58)        // 0-9    48 - 57
+    || (code > 64 && code < 91)             // A-Z      65 - 90
+    || (code > 96 && code < 123  ) )    // a-z        97 - 122 
+}
+
+var toLower = function (char) {
+    return char < 97 ? char + 32 : char;
+}
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'));        // true
+console.log(isPalindrome('race a car'));        // false
+console.log(isPalindrome(' '));      // true
+console.log(isPalindrome('.,'))       // true
